7a95c2f40663d18d64c2ac2c430b8ef8ba56f297
Carl Saginsin <saguinsincarl8@gmail.com>
2024-02-17 21:47:18 +0700

    17/02/2024 10:00 pm updates
    Haven't finished this yet.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 
+
+
+// components
+import Home from './landingPage/Home'
+import Navbar from './navbar/Navbar'
+
 function App() {
-
-
   return (
     <>
+      <Navbar />
+      <Routes>
+          <Route path="/home" element={<Home />} />
+      </Routes>
     </>
   )
 }
