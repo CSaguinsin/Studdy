03ef7686d3e2faa58252a3d14d781f6d3f93d79d
Katelyn Ebuiza <katelyn3161127@gmail.com>
2024-02-28 15:05:40 +0800

    registration-form

---
+++ src/logIn/pages/LogIn.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FaGoogle, FaFacebook } from 'react-icons/fa';
+import logo from '../images/studdy.png';
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="relative box-border flex items-center p-1 h-screen">
+      <div className="absolute top-2 left-0 right-0 p-10 text-center md:top-0">
+        <p className="text-6xl font-bold font-lexend">Welcome Back</p>
+        <p className="text-3 font-lexend">Please enter your details</p>
+      </div>
+
+      <section className="p-20 rounded-md shadow-lg bg-white relative z-15 mt-15 left-2/4 md:left-40 md:w-90">
+        <p className="text-center mb-4 text-2xl font-lexend ">Log In</p>
+
+        <div className="w-full md:w-auto flex-col mr-5">
+          <form className="space-y-4 w-auto">
+            <p className="mb-0 text-sm-3">E-mail</p> 
+            <input
+              className="rounded-lg text-sm w-full px-3 py-2 border border-solid border-gray-300 rounded focus:outline-none focus:border-blue-500"
+              type="text"
+            />
+            <p className="mb-0 text-sm font-abc">Password</p> 
+            <input
+              className="rounded-lg text-sm w-full px-3 py-2 border border-solid border-gray-300 rounded focus:outline-none focus:border-blue-500"
+              type="password"
+            />
+
+            <div className="flex justify-end text-sm">
+              <a
+                className="font-lexend text-gray-400 hover:text-gray-700 hover:underline hover:underline-offset-4"
+                href="#"
+              >
+                Forgot Password?
+              </a>
+            </div>
+
+            <div className="text-base text-black-500 text-center md:text-left">
+              <div className="mb-4 text-center flex justify-center">
+                <a
+                  className="flex items-center justify-center text-black-300 border border-solid border-black rounded px-2 py-2 hover:bg-gray-200 w-full md:w-80"
+                  href="#"
+                >
+                  <FaGoogle className="mr-3 text-lg font-abc" />
+                  Sign in with Google
+                </a>
+              </div>
+              <div className="flex justify-center ">
+                <a
+                  className="flex items-center justify-center text-black-300 border border-solid border-black rounded px-2 py-2 hover:bg-gray-200 w-full md:w-80"
+                  href="#"
+                >
+                  <FaFacebook className="mr-3 text-lg font-abc" />
+                  Sign in with Facebook
+                </a>
+              </div>
+            </div>
+            
+            <button className="border-2 border-gray-400 bg-blue-600 hover:bg-blue-700 shadow-md py-2 min-w-full text-white rounded-lg text-lg tracking-wider font-lexend-deca" type="submit">
+              Log in
+            </button>
+
+            <div className="flex justify-center items-center text-sm text-center">
+              <a onClick={() => navigate('/signin')}
+                className="text-gray-400 hover:text-gray-700 underline font-lexend"
+                href="#"
+              >
+                OR SIGN UP
+              </a>
+            </div>
+          </form>
+        </div>
+      </section>
+      <div className="absolute top-50 right-1 md:w-3/5"> 
+        <img src={logo} alt="Logo" className="w-full max-h-48 md:max-h-full h-auto" /> 
+</div>
+
+
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/logIn/pages/SignIn.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FaGoogle, FaFacebook } from 'react-icons/fa';
+import logo from '../images/Sstuddy.png';
+
+const SignIn = () => {
+    const navigate = useNavigate();
+  return (
+    
+
+    <div className="relative flex items-center h-screen">
+      <div className="w-2/3 relative top-10  left-1/3 totransform -translate-x-2/4 translate-y-2/6">
+        <img src={logo} alt="Logo" className="" />
+      </div>
+
+      <div className="w-full max-w-md mx-auto text-center md:text-left">
+        <div className="absolute top-10 left-20 right-0 p-10 text-center md:top-2/8 md:transform -translate-y-1/3 space-y-3">
+          <p className="text-6xl font-bold font-lexend ">Sign Up Today!</p>
+          <p className="text-3 font-lexend">Together, we make studying way more fun</p>
+        </div>
+
+
+        <section className="p-10 shadow-lg bg-black text-white absolute right-40 top-40 bottom-20 w-full md:w-2/6 rounded-t-none rounded-r-xl rounded-l-xl rounded-b-none" >
+          <div className="w-full md:w-auto flex-col">
+            <form className="space-y-5 w-full">
+              <p className="text-center mb-0 text-2xl ">Register</p>
+              <p className="mb-0  text-sm">Name</p>
+
+              <input
+                className="rounded-lg text-sm w-full px-3 py-2 border border-solid border-gray-300 bg-gray-400 text-black focus:outline-none focus:border-gray-300"
+                type="text"
+              />
+
+              <p className="mb-0 text-sm">Password</p>
+              <input
+                className="rounded-lg text-sm w-full px-3 py-2 border border-solid border-gray-300 bg-gray-400 text-black focus:outline-none focus:border-gray-300"
+                type="password"
+              />
+
+              <p className="mb- text-sm">Re-Type Password</p>
+              <input
+                className="rounded-lg text-sm w-full px-3 py-2 border border-solid border-gray-300 bg-gray-400 text-black focus:outline-none focus:border-gray-300"
+                type="password"
+              />
+
+              <div className="text-base text-gray text-center md:text-left">
+                <div className="mb-5 text-center flex justify-center">
+
+                  <a
+                    className="flex items-center justify-center text-gray border border-solid rounded px-2 py-2 hover:bg-gray-800 w-full md:w-80"
+                    href="#"
+                  >
+                    <FaGoogle className="mr-2 text-lg font-abc" />
+                    Sign in with Google
+                  </a>
+                </div>
+
+                <div className="flex justify-center ">
+                  <a
+                    className="flex items-center justify-center text-white border border-solid rounded px-2 py-2 hover:bg-gray-800 w-full md:w-80"
+                    href="#"
+                  >
+                    <FaFacebook className="mr-2 text-lg font-abc " />
+                    Sign in with Facebook
+                  </a>
+                </div>
+
+              </div>
+            
+              <button className="  border-2 border-white bg-blue-600 hover:bg-blue-800 shadow-md py-2 min-w-full text-white rounded-lg text-lg">
+                Register
+              </button>
+
+              <div className="flex justify-center items-center text-sm text-center">
+              <a onClick={() => navigate(-2)}
+                className="text-gray-400 hover:text-gray-700 underline font-abc"
+                href="#"
+              >
+                GO BACK
+              </a>
+            </div>
+            </form>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
